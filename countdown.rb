ac437ee30650d5dc4dc408b6a96897fe016c7e7f
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-10 13:37:55 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(num)
+    while num > 0 
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        
+    end 
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+    sleep(5)
+end
+
